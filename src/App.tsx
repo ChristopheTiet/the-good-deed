62d1236b8069f8c1548fc3c38f3fae3418a469ac
Christophe Tiet <christophe@christophes-macbook-pro.home>
2022-05-04 01:38:43 +0200

    feat: delete options & handle pick

--- src/App.tsx
+++ src/App.tsx
@@ -16,14 +16,21 @@ class Header extends Component<HeaderProps> {
   }
 }
 
-class Action extends Component {
-  handlePick() {
-    alert('HandlePick');
-  }
+type ActionProps = {
+  hasOptions: boolean;
+  handlePick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+class Action extends Component<ActionProps>{
   render() {
     return (
       <div>
-        <button onClick={this.handlePick}>What should I do?</button>
+        <button 
+          onClick={this.props.handlePick}
+          disabled={!this.props.hasOptions}
+        >
+          What should I do?
+        </button>
       </div>
     )
   }
@@ -31,21 +38,14 @@ class Action extends Component {
 
 type OptionsProps = {
   options: string[];
+  handleDeleteOptions: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 class Options extends Component<OptionsProps> {
-  constructor(props: OptionsProps | Readonly<OptionsProps>) {
-    super(props);
-    this.handleRemoveAll = this.handleRemoveAll.bind(this);
-  }
-  handleRemoveAll() {
-    console.log(this.props.options);
-  }
-
   render() {
     return (
       <div>
-        <button onClick={this.handleRemoveAll}>Remove all</button>
+        <button onClick={this.props.handleDeleteOptions}>Remove all</button>
         {this.props.options.map(option => <Option key={option} optionText={option}/>)}
       </div>
     )
@@ -88,18 +88,52 @@ class Option extends Component<OptionProps> {
   }
 }
 
+type IndecisionAppProps = {}
 
-function IndecisionApp() {
-  const title: string = "Indecision App";
-  const subtitle: string = "Put your life in the hands of a computer";
-  const options: string[] = ["Thing one", "Thing two", "Thing four"]
-  return (
-   <div>
-     <Header title={title} subtitle={subtitle}/>
-     <Action/>
-     <Options options={options}/>
-     <AddOption/>
-   </div>)
+type IndecisionAppState = {
+  options: string[];
+}
+
+class IndecisionApp extends Component<IndecisionAppProps, IndecisionAppState>{
+  constructor(props: IndecisionAppProps | Readonly<IndecisionAppProps>){
+    super(props);
+    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+    this.handlePick = this.handlePick.bind(this);
+    this.state ={
+      options: [
+        "Thing one", "Thing two", "Thing four"
+      ]
+    }
+  }
+
+  handleDeleteOptions() {
+    this.setState(() => {
+      return {
+        options:[]
+      }
+    })
+  }
+
+  handlePick() {
+    const randomNum = Math.floor(Math.random()*this.state.options.length);
+    const option = this.state.options[randomNum];
+    alert(option);
+  }
+
+  render() {
+    const title: string = "Indecision App";
+    const subtitle: string = "Put your life in the hands of a computer";
+    return (
+      <div>
+        <Header title={title} subtitle={subtitle}/>
+        <Action hasOptions={this.state.options.length > 0} handlePick={this.handlePick}/>
+        <Options 
+          options={this.state.options}
+          handleDeleteOptions={this.handleDeleteOptions}
+        />
+        <AddOption/>
+      </div>)
+  } 
 }
 
 export default IndecisionApp;
